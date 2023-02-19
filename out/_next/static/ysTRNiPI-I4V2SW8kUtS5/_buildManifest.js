96ce700457f7e1607a8b7bb7a8ac34fddbedd7df
noaroos <noaroos0000@gmail.com>
2023-02-19 23:29:04 +0900

    The build directory-out commit

---
+++ out/_next/static/ysTRNiPI-I4V2SW8kUtS5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d64684d8-a9f342523c0172ef.js","static/chunks/278-1cf81be1cb53163b.js","static/chunks/pages/index-218f2d41d30b8915.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
